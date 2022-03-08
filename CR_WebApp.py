9b45f1d121cc747edf0291e4e77c3b98849a5fd9
essienhys <101192532+essienhys@users.noreply.github.com>
2022-03-08 22:54:21 +0800

    Update CR_WebApp.py

--- CR_WebApp.py
+++ CR_WebApp.py
@@ -1,6 +1,6 @@
 # Modules Import
 import streamlit as st
-from CR_Predict_Page import show_predict_page # Import Prediction Page
+
 from CR_Explore_Page import show_explore_page # Import Exploration Page
 from PIL import Image
 
@@ -17,14 +17,5 @@ with col2:
    
 st.markdown('<p class = "subheader">Developed by: Heng Yi Sheng (TP048930) <br>Supervised by: Dr. Preethi Subramanian</p>', unsafe_allow_html = True)
 
-# Create sidebar menu selection 
-menu = st.sidebar.selectbox('Predict or Explore', ('Predict', 'Explore'))
-
-# Change between prediction page or exploration page
-if menu == 'Predict':
-    st.sidebar.info('**Credit Risk Prediction Page**') # Prediction Page
-    show_predict_page() 
-else:
-    st.sidebar.info('**Credit Risk Exploration Page**') # Exploration Page
-    show_explore_page()
+show_explore_page()
     
