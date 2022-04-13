1ac0caf47616c36ad0b30478286e71d6dda37017
essienhys <101192532+essienhys@users.noreply.github.com>
2022-04-13 18:09:11 +0800

    Update CR_Explore_Page.py

--- CR_Explore_Page.py
+++ CR_Explore_Page.py
@@ -236,7 +236,7 @@ def show_explore_page():
     
     st.markdown('--------------------------')  
     
-    left, pcd, right = st.columns([10, 50, 10])
+    left, pcd, right = st.columns([5, 50, 5])
  
     # Make Parallel Category Diagram of Loan Intent, Home Ownership, Loan Grade and Historical Default
     @st.cache(max_entries = 10, ttl = 3600)
